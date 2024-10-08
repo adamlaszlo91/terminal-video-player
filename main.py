ccb160867417122426e45f9470c6a2aa57cc3f38
Ádám László <adam.laszlo.91@gmail.com>
2024-10-08 18:15:37 +0200

    Relocate resource release

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ def image_to_text(image: np.ndarray) -> str:
         text += '\n'
         for k in range(image.shape[1]):
             greyscale_value = image[i][k]
-            char_index = int((greyscale_value / 256) * (len(ascii_chars) ))
+            char_index = int((greyscale_value / 256) * (len(ascii_chars)))
             text += ascii_chars[char_index]
     return text
+
 
 def main() -> None:
     if len(sys.argv) < 2:
@@ -31,19 +32,18 @@ def main() -> None:
         ret, frame = cap.read()
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
+            cap.release()
             return
 
         image = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        
+
         # Set a correct size and aspect ration for terminal
         image = imutils.resize(image, width=80)
         image = cv.resize(image, (80, image.shape[0] // 2))
 
         image_as_text = image_to_text(image)
-        print(image_as_text, end ='', flush=True)
+        print(image_as_text, end='', flush=True)
         time.sleep(0.04)
-
-    cap.release()
 
 
 if __name__ == '__main__':
